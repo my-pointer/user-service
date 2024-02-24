72a036add5cc0ee28be36349ebcf984d77d7647e
onze <slapexs@gmail.com>
2024-02-24 23:11:37 +0700

    FIX: hande connect db env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const userDb = new Sequelize(
-	process.env.DB_NAME as string,
-	process.env.DB_USERNAME as string,
-	process.env.DB_PASSWORD as string,
-	{
-		host: process.env.DB_HOST,
-		dialect: "mysql",
-	}
-);
+const userDb = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD!, {
+	host: process.env.DB_HOST,
+	dialect: "mysql",
+});
 
 export default userDb;
 
